d24ca3470c8e95d93302555ef3467028f6f862b3
Pat York <guitarp_11@yahoo.com>
2015-02-24 10:30:59 -0800

    Added optimized shared value transfers to the GPU

---
+++ theano-deep-speech/brnngpu.py
@@ -0,0 +1,148 @@
+__author__ = 'pat'
+'''
+Bidirectional Recurrent Neural Network
+with Connectionist Temporal Classification (CTC)
+  courtesy of https://github.com/shawntan/rnn-experiment
+  courtesy of https://github.com/rakeshvar/rnn_ctc
+implemented in Theano and optimized for use on a GPU
+'''
+
+import theano
+import theano.tensor as T
+from theano_toolkit import utils as U
+from theano_toolkit import updates
+import numpy as np
+
+#THEANO_FLAGS='device=cpu,floatX=float32'
+theano.config.warn_float64='ignore'
+
+#theano.config.optimizer = 'fast_compile'
+theano.config.exception_verbosity='high'
+
+
+class FeedForwardLayer:
+    def __init__(self, inputs, input_size, output_size):
+        self.activation_fn = lambda x: T.minimum(x * (x > 0), 20)
+
+        W = U.create_shared(U.initial_weights(input_size, output_size))
+        b = U.create_shared(U.initial_weights(output_size))
+
+        self.output, _ = theano.scan(
+            lambda element: self.activation_fn(T.dot(element, W) + b),
+            sequences=[inputs]
+        )
+
+        self.params = [W, b]
+
+
+
+class RecurrentLayer:
+    def __init__(self, inputs, input_size, output_size, is_backward=False):
+        W_if = U.create_shared(U.initial_weights(input_size, output_size))
+        W_ff = U.create_shared(U.initial_weights(output_size, output_size))
+        b = U.create_shared(U.initial_weights(output_size))
+        initial = U.create_shared(U.initial_weights(output_size))
+
+        self.activation_fn = lambda x: T.minimum(x * (x > 0), 20)
+
+        def step(in_t, out_tminus1):
+            return self.activation_fn(T.dot(out_tminus1, W_ff) + T.dot(in_t, W_if) + b)
+
+        self.output, _ = theano.scan(
+            step,
+            sequences=[inputs],
+            outputs_info=[initial],
+            go_backwards=is_backward
+        )
+
+        self.params = [W_if, W_ff, b, initial]
+
+class SoftmaxLayer:
+    def __init__(self, inputs, input_size, output_size):
+        W = U.create_shared(U.initial_weights(input_size, output_size))
+        b = U.create_shared(U.initial_weights(output_size))
+
+        self.output = T.nnet.softmax(T.dot(inputs, W) + b)
+        self.params = [W, b]
+
+
+# Courtesy of https://github.com/rakeshvar/rnn_ctc
+# With T.eye() removed for k!=0 (not implemented on GPU for k!=0)
+class CTCLayer():
+    def __init__(self, inpt, labels, blank):
+        '''
+        Recurrent Relation:
+        A matrix that specifies allowed transistions in paths.
+        At any time, one could
+        0) Stay at the same label (diagonal is identity)
+        1) Move to the next label (first upper diagonal is identity)
+        2) Skip to the next to next label if
+            a) next label is blank and
+            b) the next to next label is different from the current
+            (Second upper diagonal is product of conditons a & b)
+        '''
+        n_labels = labels.shape[0]
+
+        big_I = T.cast(T.eye(n_labels+2), 'float64')
+        recurrence_relation = T.cast(T.eye(n_labels), 'float64') + big_I[2:,1:-1] + big_I[2:,:-2] * T.cast((T.arange(n_labels) % 2), 'float64')
+        recurrence_relation = T.cast(recurrence_relation, 'float64')
+
+        '''
+        Forward path probabilities
+        '''
+        pred_y = inpt[:, labels]
+
+        probabilities, _ = theano.scan(
+            lambda curr, prev: curr * T.dot(prev, recurrence_relation),
+            sequences=[pred_y],
+            outputs_info=[T.cast(T.eye(n_labels)[0], 'float64')]
+        )
+
+        # Final Costs
+        labels_probab = T.sum(probabilities[-1, -2:])
+        self.cost = -T.log(labels_probab)
+        self.params = []
+        self.debug = probabilities.T
+
+
+class BRNN:
+    def __init__(self, input_dimensionality, output_dimensionality, data_x, data_y, batch_size=1, learning_rate=0.01, momentum=.25):
+        input = T.fmatrix('input_seq')
+        label = T.ivector('label')
+        index = T.iscalar()  # index to the sample
+
+        ff1 = FeedForwardLayer(input, input_dimensionality, 200)
+        ff2 = FeedForwardLayer(ff1.output, 200, 100)
+        ff3 = FeedForwardLayer(ff2.output, 100, 50)
+        rf = RecurrentLayer(ff3.output, 50, 25, False)     # Forward layer
+        rb = RecurrentLayer(ff3.output, 50, 25, True)      # Backward layer
+        s = SoftmaxLayer(T.concatenate((rf.output, rb.output), axis=1), 2*25, output_dimensionality)
+        ctc = CTCLayer(s.output, label, output_dimensionality-1)
+
+        updates = []
+        for layer in (ff1, ff2, ff3, rf, rb, s):
+            for p in layer.params:
+                param_update = theano.shared(p.get_value()*0., broadcastable=p.broadcastable)
+                grad = T.grad(ctc.cost, p)
+                updates.append((p, p - learning_rate * param_update))
+                updates.append((param_update, momentum * param_update + (1. - momentum) * grad))
+
+        self.trainer = theano.function(
+            inputs=[index],
+            outputs=[ctc.cost, s.output],
+            updates=updates,
+            givens={
+                input: data_x[index],
+                label: data_y[index]
+            }
+        )
+
+        self.validator = theano.function(
+            inputs=[input, label],
+            outputs=[ctc.cost]
+        )
+
+        self.tester = theano.function(
+            inputs=[input],
+            outputs=[s.output]
+        )
---
+++ theano-deep-speech/gpu-cpu-test.py
@@ -0,0 +1,157 @@
+__author__ = 'pat'
+import numpy as np
+import math
+import time
+from itertools import groupby
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import cPickle as pickle
+import brnngpu as nn
+import theano
+
+#THEANO_FLAGS='device=cpu,floatX=float32'
+
+
+def str_to_seq(str):
+    seq = []
+    for c in str:
+        val = ord(c)
+        if val==32:
+            val = 26
+        elif val==39:
+            val=27
+        elif val==45:
+            val=28
+        else:
+            val-=97
+        seq.append(val)
+    return seq
+
+
+def seq_to_str(seq):
+    str = ''
+    for elem in seq:
+        if elem==26:
+            str += ' '
+        elif elem==27:
+            str += '\''
+        elif elem==28:
+            str += '-'
+        elif elem==29:
+            pass
+        else:
+            str += chr(elem+97)
+    return str
+
+
+# Remove consecutive symbols and blanks
+def F(pi, blank):
+    return [a for a in [key for key, _ in groupby(pi)] if a != blank]
+
+
+# Insert blanks between unique symbols, and at the beginning and end
+def make_l_prime(l, blank):
+    result = [blank] * (len(l) * 2 + 1)
+    result[1::2] = l
+    return result
+
+
+alphabet = np.arange(29) #[a,....,z, space, ', -]
+
+
+# Load samples
+import os
+samples = []
+directory = 'pickled'
+files = [os.path.join(directory, x) for x in os.listdir(directory)]
+files = files[:25]
+
+
+for f in files:
+    submission = pickle.load(open(f, 'rb'))
+    print f
+
+    for sample in submission:
+        label_len = len(sample[0])
+        label_prime_len = len(make_l_prime(str_to_seq(F(sample[0], len(alphabet))), len(alphabet)))
+        num_buckets = np.shape(sample[1])[0]
+        if label_len < 30 and label_prime_len <= num_buckets:# and (float(num_buckets) / float(label_prime_len) < 3.0):
+            samples.append(sample)
+
+
+
+def generate_shared(samples, blank):
+    maximum_x = np.max([s[1].shape[0] for s in samples])
+    maximum_y = np.max([len(s[0]) for s in samples])*2+1
+
+    win_x = []
+    y_data = []
+    for s in samples:
+        # Window and send
+        window_size = 1 # 1 frame of context on each side
+        windowed = []
+        for i in np.arange(window_size, len(s[1])-window_size):
+            windowed.append(np.concatenate((s[1][i-1], s[1][i], s[1][i+1])))
+
+        for j in np.arange(len(s[1]), maximum_x):
+            windowed.append(np.zeros((240)))
+
+        windowed = np.asarray(windowed, dtype=theano.config.floatX)
+        win_x.append(windowed)
+
+
+        l_p = make_l_prime(str_to_seq(F(s[0], len(alphabet))), len(alphabet))
+        l_p += [blank for x in np.arange(maximum_y-len(l_p))]
+
+        y_data.append(np.asarray(l_p, dtype='int32'))
+
+    x_data = np.rollaxis(np.dstack(win_x), -1)
+    y_data = np.asarray(y_data, dtype='int32')
+
+    print x_data.shape, x_data.dtype, x_data[0].shape
+    print y_data.shape, y_data.dtype, y_data[0].shape
+
+    shared_x_data = theano.shared(x_data, borrow=True)
+    shared_y_data = theano.shared(np.asarray(y_data), borrow=True)
+
+    return shared_x_data, shared_y_data
+
+
+shared_x, shared_y = generate_shared(samples, len(alphabet))
+
+print shared_x.dtype
+print shared_y.dtype
+
+
+
+#network = nn.BRNN(np.shape(samples[0][1])[1], len(alphabet)+1)        #x3 for the window
+network = nn.BRNN(240, len(alphabet)+1, shared_x, shared_y)        #x3 for the window
+print 'built network - num samples:', len(samples)
+
+
+minibatches = len(samples)
+
+rng = np.random.RandomState(1234)
+try:
+    for epoch in np.arange(1):
+        error = 0.0
+        for minibatch_index in np.arange(minibatches):
+            cst, pred = network.trainer(minibatch_index)
+            error += cst
+
+        print 'Epoch %i:\tAverage Error: %f' % (epoch, error/minibatches)
+
+except KeyboardInterrupt:
+    for s in samples:
+        # Window and send
+        window_size = 1 # 1 frame of context on each side
+        windowed = []
+        for i in np.arange(window_size, len(s[1])-window_size):
+            windowed.append(np.concatenate((s[1][i-1], s[1][i], s[1][i+1])))
+        
+        windowed = np.asarray(windowed)
+        #windowed=s[1]
+
+        print s[0], '||', seq_to_str([np.argmax(x) for x in network.tester(windowed)[0]])
+
+raw_input('asdgf')
