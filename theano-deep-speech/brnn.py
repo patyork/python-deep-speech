d03ef37f5a0d9bf114affa63ec933a7f2ae0ca54
Pat York <guitarp_11@yahoo.com>
2015-03-20 01:49:15 -0700

    added

--- theano-deep-speech/brnn.py
+++ theano-deep-speech/brnn.py
@@ -18,7 +18,7 @@ import cPickle as pickle
 theano.config.warn_float64='ignore'
 
 #theano.config.optimizer = 'fast_compile'
-theano.config.exception_verbosity='high'
+#theano.config.exception_verbosity='high'
 
 def relu(x): return T.min(T.max(0, x), 20)
 
@@ -65,14 +65,21 @@ class RecurrentLayer:
         self.activation_fn = lambda x: T.minimum(x * (x > 0), 20)
         self.is_backward = is_backward
 
-        def step(in_t, out_tminus1):
+        def sample_step(in_t, out_tminus1):
             return self.activation_fn(T.dot(out_tminus1, self.W_ff) + T.dot(in_t, self.W_if) + self.b)
+
+        def step(sample):
+            layer_out, _ = theano.scan(
+                sample_step,
+                sequences=[sample],
+                outputs_info=[initial],
+                go_backwards=self.is_backward
+            )
+            return layer_out
 
         self.output, _ = theano.scan(
             step,
-            sequences=[inputs],
-            outputs_info=[initial],
-            go_backwards=self.is_backward
+            sequences=[inputs]
         )
 
         self.params = [self.W_if, self.W_ff, self.b]
@@ -94,7 +101,13 @@ class SoftmaxLayer:
             self.W = theano.shared(parameters['W'], name='W')
             self.b = theano.shared(parameters['b'], name='b')
 
-        self.output = T.nnet.softmax(T.dot(inputs, self.W) + self.b)
+        #self.output = inputs
+
+        self.output, _ = theano.scan(
+            lambda x: T.nnet.softmax(T.dot(x, self.W) + self.b),
+            sequences=[inputs]
+        )
+
         self.params = [self.W, self.b]
         
     def get_parameters(self):
@@ -117,35 +130,42 @@ class CTCLayer():
             b) the next to next label is different from the current
             (Second upper diagonal is product of conditons a & b)
         '''
-        n_labels = labels.shape[0]
-        
+        n_labels = labels[0].shape[0]
+
         big_I = T.cast(T.eye(n_labels+2), 'float64')
         recurrence_relation = T.cast(T.eye(n_labels), 'float64') + big_I[2:,1:-1] + big_I[2:,:-2] * T.cast((T.arange(n_labels) % 2), 'float64')
         recurrence_relation = T.cast(recurrence_relation, 'float64')
-        
-        '''
-        Forward path probabilities
-        '''
-        pred_y = inpt[:, labels]
 
-        probabilities, _ = theano.scan(
-            lambda curr, prev: curr * T.dot(prev, recurrence_relation),
-            sequences=[pred_y],
-            outputs_info=[T.cast(T.eye(n_labels)[0], 'float64')]
+
+        def step(input, label):
+            '''
+            Forward path probabilities
+            '''
+            big_I = T.cast(T.eye(n_labels+2), 'float64')
+            recurrence_relation = T.cast(T.eye(n_labels), 'float64') + big_I[2:,1:-1] + big_I[2:,:-2] * T.cast((T.arange(n_labels) % 2), 'float64')
+            recurrence_relation = T.cast(recurrence_relation, 'float64')
+            pred_y = input[:, label]
+
+            probabilities, _ = theano.scan(
+                lambda curr, prev: curr * T.dot(prev, recurrence_relation),
+                sequences=[pred_y],
+                outputs_info=[T.cast(T.eye(n_labels)[0], 'float64')]
+            )
+            return -T.log(T.sum(probabilities[-1, -2:]))
+
+        probs, _ = theano.scan(
+            step,
+            sequences=[inpt, labels]
         )
 
-
-        # Final Costs
-        labels_probab = T.sum(probabilities[-1, -2:])
-        self.cost = -T.log(labels_probab)
+        self.cost = T.mean(probs)
         self.params = []
-        self.debug = probabilities.T
 
 
 class BRNN:
     def __init__(self, input_dimensionality, output_dimensionality, params=None, learning_rate=.01, momentum_rate=.25, data_x=None, data_y=None):
-        inputs = T.matrix('input_seq')
-        labels = T.ivector('labels')
+        inputs = T.ftensor3('input_seq')
+        labels = T.imatrix('labels')
         #index = T.iscalar('index')
         
         if params is None:
@@ -154,46 +174,41 @@ class BRNN:
             self.ff3 = FeedForwardLayer(self.ff2.output, 2000, 500)
             self.rf = RecurrentLayer(self.ff3.output, 500, 300, False)     # Forward layer
             self.rb = RecurrentLayer(self.ff3.output, 500, 300, True)      # Backward layer
-            self.s = SoftmaxLayer(T.concatenate((self.rf.output, self.rb.output), axis=1), 2*300, output_dimensionality)
+            self.s = SoftmaxLayer(T.concatenate((self.rf.output, self.rb.output), axis=2), 2*300, output_dimensionality)
         else:
             self.ff1 = FeedForwardLayer(inputs, input_dimensionality, 3000, params[0])
             self.ff2 = FeedForwardLayer(self.ff1.output, 3000, 2000, params[1])
             self.ff3 = FeedForwardLayer(self.ff2.output, 2000, 500, params[2])
             self.rf = RecurrentLayer(self.ff3.output, 500, 300, False, params[3])     # Forward layer
             self.rb = RecurrentLayer(self.ff3.output, 500, 300, True, params[4])      # Backward layer
-            self.s = SoftmaxLayer(T.concatenate((self.rf.output, self.rb.output), axis=1), 2*300, output_dimensionality, params[5])
+            self.s = SoftmaxLayer(T.concatenate((self.rf.output, self.rb.output), axis=2), 2*300, output_dimensionality, params[5])
             
         ctc = CTCLayer(self.s.output, labels, output_dimensionality-1)
         l2 = T.sum(self.ff1.W**2) + T.sum(self.ff2.W**2) + T.sum(self.ff3.W**2) + T.sum(self.s.W**2) + T.sum(self.rf.W_if**2) + T.sum(self.rf.W_ff**2) + T.sum(self.rb.W_if**2) + T.sum(self.rb.W_ff**2)
 
         updates = []
-        for layer in (self.ff1, self.ff2, self.ff3, self.rf, self.rb, self.s):
-            for p in layer.params:
-                param_update = theano.shared(p.get_value()*0., broadcastable=p.broadcastable)
-                grad = T.grad(ctc.cost - .005*l2, p)
-                updates.append((p, p-learning_rate*param_update))
-                updates.append((param_update, momentum_rate*param_update + (1. - momentum_rate)*grad))
+        if not T.isnan(ctc.cost):
+            for layer in (self.ff1, self.ff2, self.ff3, self.rf, self.rb, self.s):
+                for p in layer.params:
+                    param_update = theano.shared(p.get_value()*0., broadcastable=p.broadcastable)
+                    grad = T.grad(ctc.cost - .005*l2, p)
+                    updates.append((p, p-learning_rate*param_update))
+                    updates.append((param_update, momentum_rate*param_update + (1. - momentum_rate)*grad))
 
         self.trainer = theano.function(
             inputs=[inputs, labels],
-            #inputs=[index],
             outputs=[ctc.cost, self.s.output],
-            updates=updates,
-            #givens=
-            #    {
-            #        inputs: data_x[index],
-            #        labels: data_y[index]
-            #    }
+            updates=updates
         )
 
-        self.validator = theano.function(
+        '''self.validator = theano.function(
             inputs=[inputs, labels],
             outputs=[ctc.cost]
-        )
+        )'''
 
         self.tester = theano.function(
             inputs=[inputs],
-            outputs=[self.s.output]
+            outputs=[self.rf.output, self.rb.output, self.s.output]
         )
     
     def dump(self, f_path):
---
+++ theano-deep-speech/gpu-batch.py
@@ -0,0 +1,188 @@
+__author__ = 'pat'
+import numpy as np
+import math
+import time
+from itertools import groupby
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import cPickle as pickle
+import theano
+
+import brnn as nn
+
+
+def str_to_seq(str):
+    seq = []
+    for c in str:
+        val = ord(c)
+        if val==32:
+            val = 26
+        elif val==39:
+            val=27
+        elif val==45:
+            val=28
+        else:
+            val-=97
+        seq.append(val)
+    return seq
+    
+
+def seq_to_str(seq):
+    str = ''
+    for elem in seq:
+        if elem==26:
+            str += ' '
+        elif elem==27:
+            str += '\''
+        elif elem==28:
+            str += '-'
+        elif elem==29:
+            pass
+        else:
+            str += chr(elem+97)
+    return str
+    
+
+# Remove consecutive symbols and blanks
+def F(pi, blank):
+    return [a for a in [key for key, _ in groupby(pi)] if a != blank]
+    
+
+# Insert blanks between unique symbols, and at the beginning and end
+def make_l_prime(l, blank):
+    result = [blank] * (len(l) * 2 + 1)
+    result[1::2] = l
+    return result
+    
+    
+def log_it(f, epoch=None, error=None, etime=None, samples=None, nan=False, etc=None):
+    s5 = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+    if epoch is not None:
+        message = time.strftime('%H:%M:%S') + '\tEpoch: ' + str(epoch) + '\tAvg. Error: ' + str(error / samples) + '\tin ' + str(etime) + 's\t' +str(samples) + ' samples\tSamples/sec: ' + str(samples/etime) + '\tApprox. Speed: ' + str(samples * 5 / etime) + 'x real-time'
+        print '\n', message, '\n'
+        f.write(message.replace('\t', s5) + '<br />')
+    elif nan:
+        print time.strftime('%H:%M:%S') + '\n========\nHIT NAN=======\nInvalidating Epoch\n===========\n'
+        f.write('<br />' + time.strftime('%H:%M:%S') + '\n========\nHIT NAN=======\nInvalidating Epoch\n===========\n\n<br />')
+        
+    else:
+        print etc
+        f.write(time.strftime('%H:%M:%S') +s5+ etc.replace('\n', '<br />').replace('\t', s5) + '<br />')
+
+
+alphabet = np.arange(29) #[a,....,z, space, ', -]
+
+
+# Load samples
+f = open('win3_l35.pkl', 'rb')
+samples = pickle.load(f)
+f.close()
+
+print [len(s[0]) for s in samples]
+
+samples_sorted = sorted(samples, key=lambda s: len(s[0]))
+
+lens = [len(s[0]) for s in samples_sorted]
+print lens
+
+lp_lens = [key for key, _ in groupby(lens)]
+print lp_lens
+print
+
+batch_dict = {}
+for length in lp_lens:
+    # Get samples of length
+    sample_of_len = []
+    label_of_len = []
+    for s in samples:
+        if len(s[0]) == length:
+            sample_of_len.append(np.asarray(s[1], dtype=theano.config.floatX))
+            label_of_len.append(np.asarray(s[0], dtype='int32'))
+
+    pad_to = np.max([s.shape[0] for s in sample_of_len])
+    padded = []
+    for s in sample_of_len:
+
+        pad = np.zeros((pad_to-s.shape[0], s.shape[1]))
+        padded.append( np.concatenate((s, pad), axis=0) )
+
+    #print type(padded), type(padded[0])
+    #print type(label_of_len), type(label_of_len[0])
+
+    #for s in padded:
+    #    print s.shape
+
+    batch_dict[length] = (np.asarray(label_of_len, dtype='int32'), np.asarray(padded, dtype=theano.config.floatX))
+
+
+visual_sample = samples[0]
+visual_sample2 = samples[1]
+print 'Shape of first input:', np.shape(visual_sample[1])
+print seq_to_str(visual_sample[0])
+print seq_to_str(visual_sample2[0])
+
+
+# PARAMETERS
+epoch_size = 10        # mini-batches per epoch (model is stored at the end of this many mini-batches)
+batch_size = 100        # mini-batch size
+
+# HYPER-PARAMETERS
+learning_rate = .01
+momentum_coefficient = .25
+
+
+net = nn.Network()
+rng = np.random.RandomState(int(time.time()))
+
+# create network
+try:
+    last_good = -1
+    restart = False
+    #log_file = file('/var/www/html/status.html', 'a')
+    log_file = file('status.html', 'a')
+    duration = time.time()
+    if last_good == -1:
+        network = net.create_network(560, len(alphabet)+1, .0005, .75)        #x3 for the window
+        log_it(log_file, etc='created Network - num samples:' + str(len(samples)) + '\tDuration: ' + str(time.time()-duration))
+    else:
+        picklePath = 'saved_models_batch/' + str(last_good) + '.pkl'
+        print 'loading from', picklePath
+        network = net.load_network(picklePath, 560, len(alphabet)+1, .0005, .75)        #x3 for the window
+        log_it(log_file, etc='loaded Network - num samples:' + str(len(samples)) + '\tDuration: ' + str(time.time()-duration))
+    log_file.close()
+
+    # Start a new Epoch
+    for epoch in np.arange(last_good+1, 100000):
+
+        # For each desired mini-batch
+        for minibatch in np.arange(epoch_size):
+
+            sequence_length_index = rng.randint(0, len(lp_lens))                                # randomly select a bucket of samples to create a mini-batch from
+            sequence_length = lp_lens[sequence_length_index]
+
+            num_samples_in_selected = batch_dict[sequence_length][1].shape[0]    # get the number of available samples in the bucket
+            print batch_dict[sequence_length][1].shape
+            if num_samples_in_selected < batch_size:
+                minibatch_start = 0
+                minibatch_end = num_samples_in_selected
+            else:
+                minibatch_start = rng.randint(0, len(lp_lens))
+                minibatch_end = minibatch_start + batch_size
+
+
+            cost, pred = network.trainer(batch_dict[sequence_length][1][minibatch_start:minibatch_end, :, :], batch_dict[sequence_length][0][minibatch_start:minibatch_end, :])
+
+except KeyboardInterrupt:
+    pass
+
+# shuffle lp_lens
+
+# take lp_lens[0] as length
+
+    # random from [0, batch_dict[length]-batch_size]
+
+    # batch train on the random slice
+
+    # every Xth training batch, save model, shuffle
+
+
